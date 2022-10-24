71be25d9165d8823e4e6f2ae4a6384cba4c5c566
RyanP <ryanp@javs.com>
2022-10-24 13:26:11 -0400

    Alpha 1.0 Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace VSW2200Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCurrentInput = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudLowInput = new System.Windows.Forms.NumericUpDown();
+            this.nudHighInput = new System.Windows.Forms.NumericUpDown();
+            this.lblrcv = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLowInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHighInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCurrentInput
+            // 
+            this.lblCurrentInput.AutoSize = true;
+            this.lblCurrentInput.Location = new System.Drawing.Point(210, 0);
+            this.lblCurrentInput.Name = "lblCurrentInput";
+            this.lblCurrentInput.Size = new System.Drawing.Size(62, 13);
+            this.lblCurrentInput.TabIndex = 0;
+            this.lblCurrentInput.Text = "Not Yet Set";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lblCurrentInput, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.nudLowInput, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.nudHighInput, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblrcv, 1, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(375, 111);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current Input:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time Between Input Changes (Seconds):";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(210, 16);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "First Input:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Last Input:";
+            // 
+            // nudLowInput
+            // 
+            this.nudLowInput.Location = new System.Drawing.Point(210, 42);
+            this.nudLowInput.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudLowInput.Name = "nudLowInput";
+            this.nudLowInput.Size = new System.Drawing.Size(120, 20);
+            this.nudLowInput.TabIndex = 6;
+            this.nudLowInput.ValueChanged += new System.EventHandler(this.nudLowInput_ValueChanged);
+            // 
+            // nudHighInput
+            // 
+            this.nudHighInput.Location = new System.Drawing.Point(210, 68);
+            this.nudHighInput.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudHighInput.Name = "nudHighInput";
+            this.nudHighInput.Size = new System.Drawing.Size(120, 20);
+            this.nudHighInput.TabIndex = 7;
+            this.nudHighInput.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudHighInput.ValueChanged += new System.EventHandler(this.nudHighInput_ValueChanged);
+            // 
+            // lblrcv
+            // 
+            this.lblrcv.AutoSize = true;
+            this.lblrcv.Location = new System.Drawing.Point(210, 91);
+            this.lblrcv.Name = "lblrcv";
+            this.lblrcv.Size = new System.Drawing.Size(0, 13);
+            this.lblrcv.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 111);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLowInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHighInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCurrentInput;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudLowInput;
+        private System.Windows.Forms.NumericUpDown nudHighInput;
+        private System.Windows.Forms.Label lblrcv;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VSW2200Control
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort port = null;
+        private Timer cycleTimer = new Timer();
+        private string portName = "COM1";
+        private int currentInput = 0;
+        private int lowInput = 0;
+        private int highInput = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Attach a method to be called when there
+            // is data waiting in the port's buffer
+            nudLowInput.Value = int.Parse(System.Configuration.ConfigurationManager.AppSettings["LowInput"]);
+            nudHighInput.Value = int.Parse(System.Configuration.ConfigurationManager.AppSettings["HighInput"]);
+            portName = System.Configuration.ConfigurationManager.AppSettings["COMPORT"];
+            numericUpDown1.Value = int.Parse(System.Configuration.ConfigurationManager.AppSettings["Interval"]);
+            currentInput = highInput;
+
+            cycleTimer.Interval = (int)numericUpDown1.Value * 1000;
+            cycleTimer.Tick += CycleTimer_Tick;
+            cycleTimer.Start();
+            CycleTimer_Tick(this, EventArgs.Empty);
+
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void CycleTimer_Tick(object sender, EventArgs e)
+        {
+            if (currentInput < highInput)
+                SetInput(currentInput + 1);
+            else
+                SetInput(lowInput);
+        }
+
+        void OpenPort()
+        {
+            if (port == null)
+            {
+                port = new SerialPort(portName, 115200, Parity.None, 8, StopBits.One);
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            }
+            if (!port.IsOpen)
+                port.Open();
+        }
+
+        void ClosePort()
+        {
+            if (port.IsOpen)
+                port.Close();
+        }
+        string rcv = "";
+        private void port_DataReceived(object sender,
+  SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            rcv = port.ReadExisting();
+        }
+
+
+        void SetInput(int input)
+        {
+            if (input > 3 || input < 0)
+                return;
+            List<byte> cmd = new List<byte>();
+            cmd.Add(0x05);
+            cmd.Add(0x90);
+            cmd.Add(0x00);
+            cmd.Add((byte)input);
+            int chksum = 0;
+            for (int i = 0; i < cmd.Count; i++)
+            {
+                chksum = chksum + cmd[i];
+            }
+            cmd.Add((byte)chksum);
+            OpenPort();
+            port.Write(cmd.ToArray<byte>(), 0, cmd.Count);
+            currentInput = input;
+            lblCurrentInput.Text = currentInput.ToString();
+            //Command Acknowledgment
+            //0 ACK Length 0x03
+            //1 ACK 0x81
+            //2 ACK Check sum 0x84
+            //3 ACK Length 0x03
+            //4 ACK 0x82
+            //5 ACK Check sum 0x85
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            cycleTimer.Stop();
+            cycleTimer.Interval = (int)numericUpDown1.Value * 1000;
+            cycleTimer.Start();
+        }
+
+        private void nudLowInput_ValueChanged(object sender, EventArgs e)
+        {
+            if (nudLowInput.Value > nudHighInput.Value)
+                nudLowInput.Value = nudHighInput.Value;
+            lowInput = (int)nudLowInput.Value;
+        }
+
+        private void nudHighInput_ValueChanged(object sender, EventArgs e)
+        {
+            if (nudHighInput.Value < nudLowInput.Value)
+                nudHighInput.Value = nudLowInput.Value;
+            highInput = (int)nudHighInput.Value;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                char[] charValues = rcv.ToCharArray();
+                string hexOutput = "";
+                foreach (char _eachChar in charValues)
+                {
+                    // Get the integral value of the character.
+                    int value = Convert.ToInt32(_eachChar);
+                    // Convert the decimal value to a hexadecimal value in string form.
+                    hexOutput += "0x" + String.Format("{0:X}", value) + " ";
+                    // to make output as your eg 
+                    //  hexOutput +=" "+ String.Format("{0:X}", value);
+
+                }
+
+                lblrcv.Text = hexOutput;
+            }
+            catch { }
+        }
+    }
+}
